52fc9dce1b9135898ae9d74e81322753f213fdc9
hngochanh <hngochanh14@gmail.com>
2016-10-11 23:21:39 +0700

    Delete file thừa
    Signed-off-by: hngochanh <hngochanh14@gmail.com>

--- SampleProject/WindowBasics/Helloworld/Source.cpp
+++
@@ -1,9 +0,0 @@
-#include <Windows.h>
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
-{
-	MessageBox(NULL, "Motoko Kusanagi has hacked your system!",
-		"Public Security Section 9", MB_YESNO | MB_ICONEXCLAMATION);
-}
-
-
